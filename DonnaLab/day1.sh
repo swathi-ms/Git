cfb4a8ce8ac43fb1f3771d75156ea6954efd65ee
Swathi Munikoti <swathi_ms@live.in>
2023-08-17 15:19:05 -0400

    First Commit

---
+++ DonnaLab/day1.sh
@@ -0,0 +1,5 @@
+sudo yum install  tree
+mkdir PlanetGreen
+mkdir -p PlanetGreen/Earth
+mkdir -p PlanetGreen/Fire PlanetGreen/Wind PlanetGreen/Water PlanetGreen/Heart
+mkdir -p PlanetGreen/Earth/IT/Management PlanetGreen/Earth/IT/Engineering PlanetGreen/Earth/IT/Systems\ Administration PlanetGreen/Earth/IT/Security PlanetGreen/Earth/IT/Networking
