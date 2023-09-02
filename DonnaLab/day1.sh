f87c7b9be904f2566e447a50d243f1410510af42
Swathi Munikoti <swathi_ms@live.in>
2023-09-02 12:28:05 -0700

    Just making sure the changes are reflected on Github.

--- DonnaLab/day1.sh
+++
@@ -1,5 +0,0 @@
-sudo yum install  tree
-mkdir PlanetGreen
-mkdir -p PlanetGreen/Earth
-mkdir -p PlanetGreen/Fire PlanetGreen/Wind PlanetGreen/Water PlanetGreen/Heart
-mkdir -p PlanetGreen/Earth/IT/Management PlanetGreen/Earth/IT/Engineering PlanetGreen/Earth/IT/Systems\ Administration PlanetGreen/Earth/IT/Security PlanetGreen/Earth/IT/Networking
