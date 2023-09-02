848d879e92f2827bdfb2bede8c36286687849d42
Swathi Munikoti <swathi_ms@live.in>
2023-09-02 15:06:16 -0700

    Adding 2 filed : DownloadingTree and AddingUsers.

---
+++ DonnaLab/DownloadingTree.sh
@@ -0,0 +1,8 @@
+echo "in the DownloadingTree.sh. About to install tree command"
+
+sudo yum install git -y
+git clone https://github.com/swathi-ms/Git.git
+cd Git/DonnaLab/
+chmod 777 DownloadingTree.sh
+ls -l
+sudo yum install  tree
