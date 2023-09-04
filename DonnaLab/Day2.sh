0f55e0ffe2499aa0acd4fdee5425c3d507f3caee
Swathi Munikoti <swathi_ms@live.in>
2023-09-03 23:59:41 -0700

    Adding users in DEIB

--- DonnaLab/Day2.sh
+++ DonnaLab/Day2.sh
@@ -7,7 +7,14 @@ sudo groupadd DEIB
 
 
 groupname="DEIB"
-usernames="ASjohnson,ENgraham,DSsanchez,XJhurston,RKjames,KAcorinth,RRwinthrow,TYCorson"
+usernames=("ASjohnson" "ENgraham" "DSsanchez" "XJhurston" "RKjames" "KAcorinth" "RRwinthrow" "TYCorson")
 
 # Use the usermod command to add users to the group
-sudo usermod -aG "$groupname" "$usernames"
+#sudo usermod -aG "$groupname" "$usernames"
+
+# Loop through the array and add each user to the group
+for username in "${usernames[@]}"; do
+    sudo usermod -aG "$groupname" "$username"
+done
+
+tail -50 /etc/group
